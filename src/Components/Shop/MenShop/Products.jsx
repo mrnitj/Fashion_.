a26529edaa65ab128415346872230cf24709dd2e
mrnitj <nithinraj982@gmail.com>
2024-10-16 00:06:58 +0530

    updated

--- src/Components/Shop/MenShop/Products.jsx
+++ src/Components/Shop/MenShop/Products.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { productDatas } from "../../../Datas/ProductDatas";
 import ProductCard from "../ProductCard";
 
-import { Box, Grid, styled } from "@mui/material";
+import { Box, Grid2, styled } from "@mui/material";
 import FilterBox from "./FilterBox";
 
 const MainContainer = styled(Box)(({ theme }) => ({
@@ -17,9 +17,9 @@ const FilterContainer = styled(Box)(({ theme }) => ({
 }));
 
 const ProductContainer = styled(Box)(({ theme }) => ({}));
-const GridContainer = styled(Grid)(({ theme }) => ({
+const GridContainer = styled(Grid2)(({ theme }) => ({
 }));
-const GridItem = styled(Grid)(({ theme }) => ({
+const GridItem = styled(Grid2)(({ theme }) => ({
 border:'1px solid rgba(144, 144, 153, 0.2)'
 
 }));
@@ -33,7 +33,7 @@ const Products = () => {
             <ProductContainer>
                 <GridContainer container  p={2}>
                     {productDatas.map((item,index) => (
-                        <GridItem key={item.id} item xs={6} md={4} lg={3}>
+                        <GridItem item key={item.id}  size={{ xs: 6, md: 4, lg: 3 }}>
                             <ProductCard products={item} index={index} />
                         </GridItem>
                     ))}
