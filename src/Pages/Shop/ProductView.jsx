d63b8133b4124e7edffa054d1538d9f5bbf8510c
mrnitj <nithinraj982@gmail.com>
2024-10-17 16:28:17 +0530

    updated

--- src/Pages/Shop/ProductView.jsx
+++ src/Pages/Shop/ProductView.jsx
@@ -1,17 +1,15 @@
-import { Box, styled } from '@mui/material'
-import React from 'react'
-import MainNavbar from '../../Components/roots/MainNavbar'
-import Banner from '../../Components/Shop/ProductView/Banner'
-
-
+import { Box, styled } from "@mui/material";
+import React from "react";
+import MainNavbar from "../../Components/roots/MainNavbar";
+import Banner from "../../Components/Shop/ProductView/Banner";
 
 const ProductView = () => {
-  return (
-    <div>
-      <MainNavbar/>
-      <Banner/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <MainNavbar />
+            <Banner />
+        </div>
+    );
+};
 
-export default ProductView
+export default ProductView;
